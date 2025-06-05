678274a752940fc22a1a381b40d076e30bed937b
Danish Nasarudin <86049685+DanishNasarudin@users.noreply.github.com>
2025-06-05 13:14:18 +0800

    initial: shadcn, clerk

---
+++ lib/providers.tsx
@@ -0,0 +1,15 @@
+import ClerkProvider from "./providers/clerk-provider";
+import { ThemeProvider } from "./providers/theme-provider";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <ClerkProvider>{children}</ClerkProvider>
+    </ThemeProvider>
+  );
+}
