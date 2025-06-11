31ae903232051038e5fea94569639de6c7a75675
Danish Nasarudin <86049685+DanishNasarudin@users.noreply.github.com>
2025-06-11 17:51:10 +0800

    fix: prisma global export

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,27 @@
 import { PrismaClient } from "@/db/generated/prisma";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+declare global {
+  // allow global `prisma` across module reloads in dev
+  // @ts-ignore
+  var prisma: PrismaClient | undefined;
+}
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+// const prismaClientSingleton = () => {
+//   return new PrismaClient();
+// };
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+// declare const globalThis: {
+//   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+// } & typeof global;
 
-export default prisma;
+const prisma =
+  global.prisma ??
+  new PrismaClient({
+    log: ["warn", "error"], // optional: surface warnings/errors
+  });
 
 if (process.env.NODE_ENV !== "production") {
-  globalThis.prismaGlobal = prisma;
+  global.prisma = prisma;
 }
+
+export default prisma;
