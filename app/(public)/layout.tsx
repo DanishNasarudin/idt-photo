68c6533e1c9d4a6e0bc38115b91163f6be00b6d7
Danish Nasarudin <86049685+DanishNasarudin@users.noreply.github.com>
2026-06-20 23:47:07 +0800

    feat: add public navbar menu fallback

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,14 +1,17 @@
 import Footer from "@/components/custom/footer";
 import Navbar from "@/components/custom/navbar";
+import { getNavbarMenu } from "@/services/navbarActions";
 
-export default function PublicLayout({
+export default async function PublicLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const menuList = await getNavbarMenu();
+
   return (
     <div className="flex flex-col flex-1">
-      <Navbar />
+      <Navbar menuList={menuList} />
       {children}
       <Footer />
     </div>
